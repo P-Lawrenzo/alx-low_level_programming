d046346b5b9c0aef5ca71595e275f3c6246ed101
peter <peteruyokei@gmail.com>
2023-02-17 15:51:33 -0800

    Show alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'b';
+	while
+		(c <= 'u') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
