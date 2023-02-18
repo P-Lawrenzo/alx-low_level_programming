b12028753cfc2170bcce0ef9c24caa347d102723
peter <peteruyokei@gmail.com>
2023-02-17 20:01:36 -0800

    Different combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,13 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 0; a <= 8; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = a + 1; b <= 9; b++)
+		for (b = a + 1; b <= 10; b++)
 		{
 			putchar(a + '0');
 			putchar(b + '0');
-			if (a != 8 && b != 9)
+			if (a != 9 && b != 10)
 			{
 				putchar(',');
 				putchar(' ');
