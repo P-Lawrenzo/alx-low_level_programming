8288683e118795c15b3113a8dde03aa4669dcde5
peter <peteruyokei@gmail.com>
2023-02-19 17:55:24 -0800

    quote- standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 * main - A program that prints a line to the standard error
-*(1) Success
+*Return: (1) Success
 */
 int main(void)
 {
-	fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
