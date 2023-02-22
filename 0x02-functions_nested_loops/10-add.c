98f7c55457048b408bc99e7696e944221a8e316b
peter <peteruyokei@gmail.com>
2023-02-21 23:47:45 -0800

    Addition of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Add two vslues
+ * @n1: The first num to be computed
+ * @n2: The second num to be computed
+ * Return: Integer value
+ */
+int add(int n1, int n2)
+{
+	int sum = (n1 + n2);
+
+	return (sum);
+}
