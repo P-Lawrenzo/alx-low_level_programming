5291dcd394b5a897f894cccb7922c6507413ecde
peter <peteruyokei@gmail.com>
2023-02-24 23:01:08 -0800

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,8 +9,11 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c);
+	if (c == '2' || c == '4')
+	{
+		continue;
+	}
+	_putchar(c + '0');
 	}
 	_putchar('\n');
 }
