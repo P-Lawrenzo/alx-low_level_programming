a88fd3c1cee20280d6fb1fb1ed93f8790adb1bec
peter <peteruyokei@gmail.com>
2023-02-24 20:12:24 -0800

    Task-3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,26 +11,37 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-	{
-	if (month >= 2 && day >= 60)
-	{
-		day++;
-	}
+int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int total_days = 0;
+int i;
 
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 366 - day);
-	}
-	else
-	{
-	if (month == 2 && day == 60)
-	{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-	}
-	else
-	{
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 365 - day);
-	}
-	}
+for (i = 0; i < month - 1; i++)
+{
+total_days += days_in_month[i];
 }
+
+total_days += day;
+
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+{
+if (month > 2)
+{
+total_days++;
+}
+printf("Day of the year: %d\n", total_days);
+printf("Remaining days: %d\n", 366 - total_days);
+}
+else
+{
+if (month == 2 && day == 29)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+}
+else
+{
+printf("Day of the year: %d\n", total_days);
+printf("Remaining days: %d\n", 365 - total_days);
+}
+}
+}
+
