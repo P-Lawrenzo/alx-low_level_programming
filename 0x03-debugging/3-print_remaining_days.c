ae47e7557fe08fad4bd82e4183587f2c626eadf5
peter <peteruyokei@gmail.com>
2023-02-26 17:19:07 -0800

    Task-3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,35 +11,26 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-int days_in_feb = 28;
+    if ((year % 100 == 0 || year % 400 == 0) && !(year % 4 == 0))
+    {
+        if (month >= 2 && day >= 60)
+        {
+            day++;
+        }
 
-if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-{
-days_in_feb = 29;
+        printf("Day of the year: %d\n", day);
+        printf("Remaining days: %d\n", 366 - day);
+    }
+    else
+    {
+        if (month == 2 && day == 60)
+        {
+            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+        }
+        else
+        {
+            printf("Day of the year: %d\n", day);
+            printf("Remaining days: %d\n", 365 - day);
+        }
+    }
 }
-if (month < 1 || month > 12)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-}
-else if (day < 1 || day > 31)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-}
-else if ((month == 2 && day > days_in_feb) ||(month == 4 || month == 6 || month == 9 || month == 11) && day == 31))
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-}
-else
-{
-day = convert_day(month, day);
-
-if (days_in_feb == 28 && month >= 3)
-{
-day--;
-}
-
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 + (days_in_feb == 29) - day);
-}
-}
-
